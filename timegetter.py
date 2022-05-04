5e27fac1751cdb78f7d1849974accb6c486ef5fa
Nikita Tyutikov <tnikita17@gmail.com>
2022-05-04 16:30:01 +0300

    Добавлено узнавание времени

---
+++ timegetter.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+vrem = datetime.now().strftime("%H:%M")
+mouth = str(datetime.now().month)
+day = str(datetime.now().day)
+
+mounths = {
+'1': 'Января',
+'2': "Февраля",
+'3': "Марта",
+'4': "Апреля",
+'5': "Мая",
+'6': "Июня",
+'7': "Июля",
+'8': "Августа",
+'9': "Сентября",
+'10': "Октября",
+'11': "Ноября",
+'12': "Декабря",
+}
+
+date = day + ' ' + mounths[mouth]
