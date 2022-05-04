8f9fe21c8feff3126d2d7d2154c1fb970d2d5e61
Kirill <k746034@gmail.com>
2022-05-04 22:09:25 +0800

    v1 рабочего кода

--- network_getter.py
+++ network_getter.py
@@ -5,6 +5,7 @@ url = 'https://www.vbr.ru/banki/kurs-valut/prodaja-usd/'
 resp = requests.get(url).content
 soup = BeautifulSoup(resp, 'html.parser')
 
+
 def cleaner(text):
     ans = ''
     for i in text:
@@ -12,11 +13,13 @@ def cleaner(text):
             ans += i
     return ans
 
+
 def get_dollar(soup):
-    doll = soup.find_all('td', class_= "rates-val")
+    doll = soup.find_all('td', class_="rates-val")
     db = cleaner(doll[0].text)
     ds = cleaner(doll[1].text)
     return db, ds
+
 
 db, ds = get_dollar(soup)
 ##############################################################
@@ -24,10 +27,12 @@ url = 'https://www.vbr.ru/banki/kurs-valut/prodaja-eur/'
 resp = requests.get(url).content
 soup = BeautifulSoup(resp, 'html.parser')
 
+
 def get_euro(soup):
-    eur = soup.find_all('td', class_= "rates-val")
+    eur = soup.find_all('td', class_="rates-val")
     eb = cleaner(eur[0].text)
     es = cleaner(eur[1].text)
     return eb, es
+
 
 eb, es = get_euro(soup)
